97ee7e368bea84b5186a1da3c2e1917b08cc25cc
Tobias Oetiker <tobi@oetiker.ch>
2013-09-09 16:45:31 +0200

    initial

---
+++ build/joe/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+. ../../lib/functions.sh
+
+PROG=joe
+VER=3.7
+VERHUMAN=$VER
+PKG=text/joe
+SUMMARY="joe - Joe's own editor"
+DESC="a very flexible editor"
+MIRROR=sourceforge.net
+DLDIR=projects/joe-editor/files/JOE%20sources/$PROG-$VER
+
+BUILDARCH=32
+CONFIGURE_OPTS_32="$CONFIGURE_OPTS_32 --prefix=/opt/oep"
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+make_package
+clean_up
+
+# Vim hints
+# vim:ts=4:sw=4:et:
