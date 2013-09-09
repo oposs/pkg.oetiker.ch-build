c223f9480bd71b019cdb7008f37765c019e58718
Tobias Oetiker <tobi@oetiker.ch>
2013-09-09 17:18:56 +0200

    publish locally

--- lib/site.sh
+++ lib/site.sh
@@ -1,9 +1,9 @@
 # Package server URL and publisher
 PKGPUBLISHER=pkg.oetiker.ch
-PKGSRVR=http://pkg.oetiker.ch/
+#PKGSRVR=http://pkg.oetiker.ch/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
 # of the URI specified above.
 #
-#PKGSRVR=file:///$MYDIR/../tmp.repo/
+PKGSRVR=file:////oep-pkg/
