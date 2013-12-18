9af3488122dd5f9ea88cb8b1f6f392f993839b01
Tobias Oetiker <tobi@oetiker.ch>
2013-12-18 08:14:44 +0000

    update to 3.6.21

--- build/samba/build.sh
+++ build/samba/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=samba      # App name
-VER=3.6.18      # App version
+VER=3.6.21      # App version
 VERHUMAN=$VER   # Human-readable version
 #PVER=          # Branch (set in config.sh, override here if needed)
 PKG=service/network/samba # Package name (e.g. library/foo)
@@ -112,7 +112,7 @@ nss_install() {
 # samba does not seem to install it
 make_install32() {
     make_install
-    nss_install 
+#    nss_install 
     # remove leftover object files
     rm -rf $TMPDIR/$PROG-$VER/*/*.o
 }
