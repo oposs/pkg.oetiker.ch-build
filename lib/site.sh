158ea7574482e38fd9a5c1d5a3e27abfc2a9cd6f
Dominik Hassler <hadfl@cpan.org>
2014-11-20 19:55:40 +0100

    - minidlna config file dependency

--- lib/site.sh
+++ lib/site.sh
@@ -1,9 +1,9 @@
 # Package server URL and publisher
-PKGPUBLISHER=pkg.oetiker.ch
+PKGPUBLISHER=pkg.hadfl
 #PKGSRVR=http://pkg.oetiker.ch/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
 # of the URI specified above.
 #
-PKGSRVR=file:////oep-pkg/
+PKGSRVR=file:////hadfl-pkg/
