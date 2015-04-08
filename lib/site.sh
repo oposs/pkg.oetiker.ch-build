61b0b4865e3c047db748177bbb87e0d3c31cceb4
Dominik Hassler <hadfl@cpan.org>
2015-04-08 23:00:06 +0200

    - make it oetiker.ch comaptible

--- lib/site.sh
+++ lib/site.sh
@@ -1,9 +1,10 @@
 # Package server URL and publisher
-PKGPUBLISHER=pkg.hadfl
-#PKGSRVR=http://pkg.oetiker.ch/
+#PKGPUBLISHER=pkg.hadfl
+PKGSRVR=http://pkg.oetiker.ch/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
 # of the URI specified above.
 #
-PKGSRVR=file:////hadfl-pkg/
+#PKGSRVR=file:////hadfl-pkg/
+PKGSRVR=file:////oep-pkg/
