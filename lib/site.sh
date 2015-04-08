d52949f5d21ce82d582699475dd64dee738fe08c
Dominik Hassler <hadfl@cpan.org>
2015-04-08 23:05:59 +0200

    - another try...

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
 #PKGPUBLISHER=pkg.hadfl
-PKGSRVR=http://pkg.oetiker.ch/
+PKGPUBLISHER=pkg.oetiker.ch
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
